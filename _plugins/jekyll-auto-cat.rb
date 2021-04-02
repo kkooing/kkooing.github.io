e38396a7affd42f869dfbc262a9a904112fad260
kkooing <kdw7968@naver.com>
2021-04-02 09:16:21 +0900

    2021-04-02 09:16:21

--- _plugins/jekyll-auto-cat.rb
+++ _plugins/jekyll-auto-cat.rb
@@ -4,10 +4,9 @@
 module Jekyll
   class Document
     def categories_from_path(special_dir)
-      superdirs = relative_path.sub(special_dir, "")
+      superdirs = relative_path.sub(special_dir, "").downcase
       superdirs = superdirs.split(File::SEPARATOR)
       superdirs.reject! { |c| c.empty? || c == special_dir || c == basename }
-      puts relative_path
       merge_data!({ "categories" => superdirs }, :source => "file path")
     end
   end
