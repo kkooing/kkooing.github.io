3f9fa97e88cd4c270cd5836b879390b97d814cad
kkooing <kdw7968@naver.com>
2021-04-21 10:13:39 +0900

    2021-04-21 10:13:39

--- _site/assets/fonts/maker/t2w.py
+++
@@ -1,9 +0,0 @@
-import os
-from fontTools.ttLib import TTFont
-
-entries = os.listdir('D2Coding_subset/')
-for entry in entries:
-    print(entry)
-    f = TTFont('D2Coding_subset/' + entry)
-    f.flavor='woff2'
-    f.save('D2Coding_subset-out/' + entry.replace('.ttf', '.woff2'))
